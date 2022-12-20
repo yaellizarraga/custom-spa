bd460f4d95e9c3f8d823e4e1f53f20ff5a59d262
Yael Lizarraga <brandon.lira@dportenis.com.mx>
2022-12-20 00:18:50 -0700

    se crea grid del sitio

--- pages/proyectos/[projectId].tsx
+++ pages/proyectos/[projectId].tsx
@@ -1,17 +1,47 @@
 import * as React from 'react';
-import { createTheme, ThemeProvider, Typography } from '@mui/material';
+import { Box, Container, createTheme, Grid, Paper, styled, ThemeProvider, Typography } from '@mui/material';
 import MainMenu from '../../components/menu/menu';
 import Copyright from '../../components/copyrigth/copy';
+import CssBaseline from '@mui/material/CssBaseline'
 
 export default function ProjectDetail() {
     const theme = createTheme();
+    const Item = styled(Box)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        marginTop: '2%'
+      }));
     return (
         <ThemeProvider theme={theme}>
             <MainMenu />
+            <CssBaseline />
+            <MainMenu />
             <main>
-            <Typography>Esta será la pagina del detalle de proyecto</Typography>
+                <Container disableGutters>
+                    <Grid container spacing={2}>
+                        <Grid item md={12}>
+                            <Item>
+                               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, voluptate. Minima fuga blanditiis vel in consequuntur dolorum, quis facilis excepturi dolor nesciunt unde ad commodi modi ratione, qui, assumenda voluptates!
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1}>
+                        <Grid item md={6}>
+                            <Item>
+                               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, voluptate. Minima fuga blanditiis vel in consequuntur dolorum, quis facilis excepturi dolor nesciunt unde ad commodi modi ratione, qui, assumenda voluptates!
+                            </Item>
+                        </Grid>
+                        <Grid item md={6}>
+                            <Item>
+                               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, voluptate. Minima fuga blanditiis vel in consequuntur dolorum, quis facilis excepturi dolor nesciunt unde ad commodi modi ratione, qui, assumenda voluptates!
+                            </Item>
+                        </Grid>
+                    </Grid>
+                </Container>
             </main>
-            <Copyright />
         </ThemeProvider>
     );
 }
--- pages/proyectos/index.tsx
+++ pages/proyectos/index.tsx
@@ -1,14 +1,44 @@
 import * as React from 'react';
-import { createTheme, ThemeProvider, Typography } from '@mui/material';
+import { Box, Container, createTheme, Grid, Paper, styled, ThemeProvider, Typography } from '@mui/material';
 import MainMenu from '../../components/menu/menu';
+import CssBaseline from '@mui/material/CssBaseline'
 
 export default function ProjectDetail() {
     const theme = createTheme();
+    const Item = styled(Box)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        marginTop: '2%'
+      }));
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             <MainMenu />
             <main>
-                <Typography>Esta será la pagina de la galeria de proyectos</Typography>
+                <Container disableGutters>
+                    <Grid container spacing={2}>
+                        <Grid item md={12}>
+                            <Item>
+                               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, voluptate. Minima fuga blanditiis vel in consequuntur dolorum, quis facilis excepturi dolor nesciunt unde ad commodi modi ratione, qui, assumenda voluptates!
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1}>
+                        <Grid item md={6}>
+                            <Item>
+                               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, voluptate. Minima fuga blanditiis vel in consequuntur dolorum, quis facilis excepturi dolor nesciunt unde ad commodi modi ratione, qui, assumenda voluptates!
+                            </Item>
+                        </Grid>
+                        <Grid item md={6}>
+                            <Item>
+                               Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, voluptate. Minima fuga blanditiis vel in consequuntur dolorum, quis facilis excepturi dolor nesciunt unde ad commodi modi ratione, qui, assumenda voluptates!
+                            </Item>
+                        </Grid>
+                    </Grid>
+                </Container>
             </main>
         </ThemeProvider>
     );
